46eaf8b68cee94e2f24bdd844424a1cfd68e3b41
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-08-03 21:40:15 -0300

    change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import logo from './imagen/freecodecamp-logo.png'
 import FormularioCampo from './componente/formulario';
-import { combineReducers } from 'redux';
 import ListaDeTareas from './componente/listadetareas';
 
 function App() {
   return (
     <div className="aplicacion-tarea">
-      <div className='logo-contenedor'>
-        <img
-          src={logo}
-          className='logo' />
-      </div>
+      
       <div className='tarea-lista'>
       <h1>Mis Tareas</h1>
       <ListaDeTareas/>
